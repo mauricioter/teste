b7a4bf01db30df2e26ec74c2aa4b5cd4c3beb4c9
mauricio <mauricioterto9@gmail.com>
2023-08-14 21:51:35 -0300

    Teste

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LineChart, Grid } from 'react-native-svg-charts';
+import "ignore"
 
 export default function App() {
+  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LineChart
+        style={{ height: 200 }}
+        data={data}
+        svg={{ stroke: 'rgb(134, 65, 244)' }}
+        contentInset={{ top: 20, bottom: 20 }}
+      >
+        <Grid />
+      </LineChart>
     </View>
   );
 }
